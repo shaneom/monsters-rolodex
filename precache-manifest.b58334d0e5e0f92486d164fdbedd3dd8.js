ce72906f3e817c56fd6858d2893ada71b71cc358
Shane O'Mahoney <Shane.O'Mahoney@pdms.com>
2020-03-30 13:32:55 +0100

    Updates

---
+++ precache-manifest.b58334d0e5e0f92486d164fdbedd3dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6cea270227275d0420a5476cfdc0c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2c9ff47bc8b227d83cf2",
+    "url": "/monsters-rolodex/static/css/main.339967b4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9ff47bc8b227d83cf2",
+    "url": "/monsters-rolodex/static/js/main.cfab7554.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
